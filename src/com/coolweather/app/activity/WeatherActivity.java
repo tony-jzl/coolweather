0c17b4fa57a7d628839055441a6b68a6299af113
jzl <1076560629@qq.com>
2020-06-17 16:42:39 +0800

    加入显示天气信息功能

---
+++ src/com/coolweather/app/activity/WeatherActivity.java
@@ -0,0 +1,133 @@
+package com.coolweather.app.activity;
+
+
+import com.coolweather.app.R;
+import com.coolweather.app.util.HttpCallbackListener;
+import com.coolweather.app.util.HttpUtil;
+import com.coolweather.app.util.Utility;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class WeatherActivity extends Activity {
+
+	private LinearLayout weatherInfoLayout;
+	//显示城市名
+	private TextView cityNameText;
+	//发布时间
+	private TextView publishText;
+	//天气描述信息
+	private TextView weatherDespText;
+	//气温1
+	private TextView temp1Text;
+	//气温2
+	private TextView temp2Text;
+	//当前日期
+	private TextView currentDateText;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.weather_layout);
+		weatherInfoLayout = (LinearLayout) findViewById(R.id.weather_info_layout);
+		cityNameText = (TextView) findViewById(R.id.city_name);
+		publishText = (TextView) findViewById(R.id.publish_text);
+		weatherDespText = (TextView) findViewById(R.id.weather_desp);
+		temp1Text = (TextView) findViewById(R.id.temp1);
+		temp2Text = (TextView) findViewById(R.id.temp2);
+		currentDateText = (TextView) findViewById(R.id.current_date);
+		String countyCode = getIntent().getStringExtra("county_code");
+		if(!TextUtils.isEmpty(countyCode)){
+			publishText.setText("同步中...");
+			weatherInfoLayout.setVisibility(View.INVISIBLE);
+			cityNameText.setVisibility(View.INVISIBLE);
+			queryWeatherCode(countyCode);
+		}else{
+			showWeather();
+		}
+		
+	}
+	
+	//从SharedPreferences文件读取存储天气的信息
+	private void showWeather() {
+		// TODO Auto-generated method stub
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		cityNameText.setText(prefs.getString("city_name", ""));
+		temp1Text.setText(prefs.getString("temp1", ""));
+		temp2Text.setText(prefs.getString("temp2", ""));
+		weatherDespText.setText(prefs.getString("weather_desp", ""));
+		publishText.setText("今天"+prefs.getString("publish_time", "")+"发布");
+		currentDateText.setText(prefs.getString("current_date", ""));
+		weatherInfoLayout.setVisibility(View.VISIBLE);
+		cityNameText.setVisibility(View.VISIBLE);
+	}
+
+
+	//查询县级代号所对应的天气代号
+	private void queryWeatherCode(String countyCode) {
+		// TODO Auto-generated method stub
+		String address = "http://www.weather.com.cn/data/list3/city" + countyCode + ".xml";
+		queryFromServer(address,"countyCode");
+	}
+
+	//查询天气代号所对应的天气
+	private void queryWeatherInfo(String weatherCode) {
+		// TODO Auto-generated method stub
+		String address = "http://www.weather.com.cn/data/cityinfo/" + weatherCode + ".html";
+		queryFromServer(address,"weatherCode");
+	}
+	
+	private void queryFromServer(final String address,final String type) {
+		// TODO Auto-generated method stub
+		HttpUtil.sendHttpRequest(address, new HttpCallbackListener() {
+			
+			@Override
+			public void onFinish(final String response) {
+				// TODO Auto-generated method stub
+				if("countyCode".equals(type)){
+					if(!TextUtils.isEmpty(response)){
+						String[] array = response.split("\\|");
+						if(array != null && array.length == 2){
+							String weatherCode = array[1];
+							queryWeatherInfo(weatherCode);
+						}
+					}
+				}else if("weatherCode".equals(type)){
+					Utility.handleWeatherResponse(WeatherActivity.this, response);
+					runOnUiThread(new Runnable() {
+						
+						@Override
+						public void run() {
+							// TODO Auto-generated method stub
+							showWeather();
+						}
+					});
+					
+				}
+			}
+			
+			@Override
+			public void onError(Exception e) {
+				// TODO Auto-generated method stub
+				runOnUiThread(new Runnable() {
+					
+					@Override
+					public void run() {
+						// TODO Auto-generated method stub
+						publishText.setText("同步失败");
+					}
+				});
+			}
+		});
+	}
+	
+	
+}
